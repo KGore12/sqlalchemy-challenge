3fff5d0a57bd1985d9014fb71f70654d1d2e9c26
KGore12 <68256064+KGore12@users.noreply.github.com>
2021-01-11 15:30:45 -0500

    app.py tobs

--- app.py
+++ app.py
@@ -102,6 +102,28 @@ def stations():
     return jsonify(all_stations)
 
 
+@app.route("/api/v1.0/tobs")
+def tobs():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    session.query(Measurement.date).order_by(Measurement.date.desc()).first()
+    year_ago = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    session.close()
+    
+    # Perform a query to retrieve the data and temp scores
+    results = session.query(Measurement.date, Measurement.tobs).\
+        filter(Measurement.date >= year_ago).\
+            filter(Measurement.station == "USC00519281").all()
+    session.close()
+
+    # Convert list of tuples into normal list
+    temperature_list = list(np.ravel(results))
+
+    return jsonify(temperature_list)
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
