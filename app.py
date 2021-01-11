e30e095bae09b3efd28d300b5bd1e69318fef14a
KGore12 <68256064+KGore12@users.noreply.github.com>
2021-01-11 14:07:03 -0500

    app.py dependencies, db setup, flask welcome

--- app.py
+++ app.py
@@ -23,10 +23,15 @@ Base.prepare(engine, reflect=True)
 Measurement = Base.classes.measurement
 Station = Base.classes.station
 
+
+
+
 #################################
 # Flask Setup/Create an app
 #################################
 app = Flask(__name__)
+
+
 
 
 #################################
@@ -52,67 +57,6 @@ def welcome():
     )
 
 
-@app.route("/api/v1.0/precipitation")
-def precipitation():
-    # Create our session (link) from Python to the DB
-    session = Session(engine)
-
-    """Return a list of all Precipitation Data"""
-    # Query all Precipitation
-    results = session.query(Measurement.date, Measurement.prcp).\
-        filter(Measurement.date >= "2016-08-24").\
-        all()
-
-    session.close()
-
-    # Convert the query results to a dictionary using date as the key and prcp as the value.
-    all_precipitation = []
-    for date,prcp  in results:
-        precipitation_dict = {}
-        precipitation_dict["date"] = date
-        precipitation_dict["prcp"] = prcp
-               
-        all_precipitation.append(precipitation_dict)
-
-    return jsonify(all_precipitation)
 
 
 
-@app.route("/api/v1.0/stations")
-def stations():
-    # Create our session (link) from Python to the DB
-    session = Session(engine)
-
-    """Return a list of all Stations"""
-    # Query all Stations
-    results = session.query(Station.station).\
-                 order_by(Station.station).all()
-
-    session.close()
-
-    # Convert list of tuples into normal list
-    all_stations = list(np.ravel(results))
-
-    return jsonify(all_stations)
-
-
-
-@app.route("/api/v1.0/tobs")
-def tobs():
-    # Create our session (link) from Python to the DB
-    session = Session(engine)
-
-    session.query(Measurement.date).order_by(Measurement.date.desc()).first()
-    year_ago = dt.date(2017, 8, 23) - dt.timedelta(days=365)
-    session.close()
-    
-    # Perform a query to retrieve the data and temp scores
-    results = session.query(Measurement.date, Measurement.tobs).\
-        filter(Measurement.date >= year_ago).\
-            filter(Measurement.station == "USC00519281").all()
-    session.close()
-
-    # Convert list of tuples into normal list
-    temperature_list = list(np.ravel(results))
-
-    return jsonify(temperature_list)
